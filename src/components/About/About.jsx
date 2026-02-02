947893aa21e55efa51da2210c6aedeedf88a7130
Piyush Shakya <piyushshakya112@gmail.com>
2026-02-02 12:43:05 +0530

    updating about page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -61,7 +61,7 @@ const About = () => {
     <section
       id='about'
       ref={ref}
-      className='relative py-14 sm:py-14 md:py-14 lg:py-18 px-4 sm:px-6 md:px-8 lg:px-12 xl:px-20 2xl:px-40 min-h-screen flex items-center justify-center bg-gray-950 font-sans overflow-hidden'
+      className='relative py-10 sm:py-110 md:py-10 lg:py-12 px-4 sm:px-6 md:px-8 lg:px-12 xl:px-20 2xl:px-40 min-h-screen flex items-center justify-center bg-gray-950 font-sans overflow-hidden'
     >
       {/* Advanced Background - Responsive */}
       <div className="absolute inset-0">
